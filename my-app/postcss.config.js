c1d269f9fc60e8c20213bc0198af1e6a0a4d10a8
nelizzy <nelizzy@hotmail.com>
2020-03-27 20:15:47 +0100

    set up tailwind and postcss

--- my-app/postcss.config.js
+++ my-app/postcss.config.js
@@ -1,8 +1,8 @@
 const tailwindcss = require('tailwindcss');
 module.exports = {
     plugins: [
-        tailwindcss('./tailwind.js'),
-        require('postcss-nested'),
+        require('postcss-import'),
+        tailwindcss('./tailwind.config.js'),
         require('autoprefixer')
     ],
 };
