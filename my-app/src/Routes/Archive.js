4b5af5b4e8f73bbdc1a9ea1bc7b18d84aaa28734
nelizzy <nelizzy@hotmail.com>
2020-03-27 23:23:32 +0100

    add search bar to views

--- my-app/src/Routes/Archive.js
+++ my-app/src/Routes/Archive.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from '../Components/Header'
+import Search from '../Components/Search'
 
 export default class Archive extends Component {
   render() {
     return (
       <div>
         <Header leftIcon="chevron-left" leftRoute="/" rightIcon="settings" rightRoute="/settings" title="Archive"/>
+        <Search/>
       </div>
     )
   }
