b589d25b9fba292744921672b041114ce907ef9b
nelizzy <nelizzy@hotmail.com>
2020-03-27 23:22:05 +0100

    make default tailwind colors available

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   theme: {
-    colors: {
-      primary: "var(--color-primary)",
-      bg: {
-        primary: "var(--color-bg-primary)",
-        secondary: "var(--color-bg-secondary)"
-      },
-      text: {
-        primary: "var(--color-text-primary)",
-        secondary: "var(--color-text-secondary)",
-        button: "var(--color-text-button)"
-      },
-      action: {
-        delete: "var(--color-action-delete)"
-      }
-    },
     fontFamily: {
       main: ["Roboto", "sans-serif"],
       label: ['"Source Sans Pro"', "sans-serif"]
     },
     extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        bg: {
+          primary: "var(--color-bg-primary)",
+          secondary: "var(--color-bg-secondary)"
+        },
+        text: {
+          primary: "var(--color-text-primary)",
+          secondary: "var(--color-text-secondary)",
+          button: "var(--color-text-button)"
+        },
+        action: {
+          delete: "var(--color-action-delete)"
+        }
+      },
       gridTemplateColumns: {
         'header': '30px 1fr 30px'
       }
