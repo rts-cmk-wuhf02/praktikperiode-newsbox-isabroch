6b9371f6df5747167a0c5384c8c477913c02398b
nelizzy <nelizzy@hotmail.com>
2020-03-27 23:24:35 +0100

    move classnames from span to link

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -4,16 +4,14 @@ import { Link } from "react-router-dom";
 export default class Header extends Component {
   render() {
     return (
-      <header className="grid p-6 grid-cols-header text-center items-center text-lg text-text-primary border-solid border-b-2 border-bg-secondary">
+      <header className="grid px-4 py-6 grid-cols-header text-center items-center text-lg text-text-primary border-solid border-b-2 border-bg-secondary">
         {this.props.leftIcon ? (
-          <Link to={this.props.leftRoute}>
-            <span className={`th ${`th-${this.props.leftIcon}`}`.trim()}></span>
+          <Link to={this.props.leftRoute} className={`th ${`th-${this.props.leftIcon}`}`.trim()}>
           </Link>
         ) : null}
         <h1 className="font-bold font-main">{this.props.title}</h1>
         {this.props.rightIcon ? (
-          <Link to={this.props.rightRoute}>
-            <span className={`th ${`th-${this.props.rightIcon}`}`.trim()}></span>
+          <Link to={this.props.rightRoute} className={`th ${`th-${this.props.rightIcon}`}`.trim()}>
           </Link>
         ) : null}
       </header>
