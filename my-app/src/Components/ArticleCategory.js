7ac2bfb7193f6af74577ff1a70651d4755ef0ab1
nelizzy <nelizzy@hotmail.com>
2020-03-29 22:10:03 +0200

    style articles and categories

--- my-app/src/Components/ArticleCategory.js
+++ my-app/src/Components/ArticleCategory.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
 import ArticleSummary from "../Components/ArticleSummary";
+import categoryItem from "../media/icn_surfing.svg";
 
 export default class Category extends Component {
   render() {
     const articles = ["1", "2", "3"];
 
     return (
-      <details>
-        <summary>
-          <h2>Sport</h2>
+      <details className="category">
+        <summary className="category__dropdown flex items-center px-4 py-3 text-text-primary bordered-item">
+          <img
+            src={categoryItem}
+            width={35}
+            height={35}
+            alt=""
+            className="inline p-2 shadow-xl rounded-full"
+          />
+          <h2 className="inline pl-2 font-bold uppercase">Sport</h2>
         </summary>
         <ul>
           {articles.map(article => (
             <li>
-              <ArticleSummary article={article}/>
+              <ArticleSummary article={article} />
             </li>
           ))}
         </ul>
--- my-app/src/Components/ArticleSummary.js
+++ my-app/src/Components/ArticleSummary.js
@@ -1,15 +1,63 @@
 import React, { Component } from "react";
 
+class DeleteButton extends Component {
+  render() {
+    return (
+      <button className="absolute top-0 left-0 w-full h-full bg-action-delete text-right">
+      <span className="text-lg text-text-button pr-10 th th-trash"></span>
+      </button>
+    )
+  }
+}
+
+class ArchiveButton extends Component {
+  render() {
+    return (
+      <button className="absolute top-0 left-0 w-full h-full bg-primary text-right">
+      <span className="text-lg text-text-button pr-10 th th-bookmark-1-o"></span>
+      </button>
+    )
+  }
+}
+
+class ActionButton extends Component {
+  render() {
+    switch (this.props.action) {
+      case "delete":
+        return <DeleteButton/>;
+
+      case "archive":
+        return <ArchiveButton/>
+
+      default:
+        return null;
+    }
+  }
+}
+
+
 export default class ArticleSummary extends Component {
   render() {
     return (
-      <article>
-        <div>
-          <h3>{this.props.article}</h3>
-          <p>Description</p>
-          <img src="" alt="" width={100} height={100} />
+      <article className="relative bordered-item">
+        <div className="grid grid-cols-auto-1 px-4 pb-4 pt-3 relative z-10 bg-bg-primary w-full">
+          <div className="pl-5 pr-10 overflow-hidden">
+            <h3 className="text-text-primary font-bold truncate">
+              {this.props.article}
+            </h3>
+            <p className="text-sm text-text-secondary truncate-2line leading-snug">
+              {this.props.article}
+            </p>
+          </div>
+          <img
+            src="http://placekitten.com/100/100"
+            alt=""
+            width={70}
+            height={70}
+            className="rounded-full h-16 w-16 order-first"
+          />
         </div>
-        <button></button>
+        <ActionButton action="archive"/>
       </article>
     );
   }
