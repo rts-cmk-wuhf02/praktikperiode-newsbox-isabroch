31441c61f663e825c51fbc0ba0bd1fb875f41640
Isabella Broch <43812480+isabroch@users.noreply.github.com>
2020-03-30 14:34:53 +0200

    Add search page and bar, no functionality
    * create basic search bar component
    * add search bar to views
    * direct to search page

---
+++ my-app/src/Components/Search.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+
+export default class Search extends Component {
+  render() {
+    return (
+      <form className="p-4 border-solid border-b-2 border-bg-secondary">
+        <div className="flex bg-bg-secondary w-full rounded font-main font-light text-text-primary items-center px-2">
+          <input type="search" name="searchResults" placeholder="Search news" className="flex-auto py-2 px-4 placeholder-text-secondary bg-transparent placeholder-op"/>
+          <Link to="/search" className="p-2 th th-loupe-o text-text-secondary" />
+        </div>
+      </form>
+    )
+  }
+}
--- my-app/src/Routes/Newsbox.js
+++ my-app/src/Routes/Newsbox.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from '../Components/Header'
+import Search from '../Components/Search'
 
 export default class Newsbox extends Component {
   render() {
     return (
       <div>
         <Header leftIcon="bookmark-1" leftRoute="/archive" rightIcon="settings" rightRoute="/settings" title="Newsbox"/>
+        <Search/>
       </div>
     )
   }
